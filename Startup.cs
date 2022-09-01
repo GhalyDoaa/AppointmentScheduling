58b7f1946ec83d56614103740135602040d1bf69
Doaa Yasser <doaaghaly397@gmail.com>
2022-09-01 13:15:19 +0200

    Update Views

--- Startup.cs
+++ Startup.cs
@@ -73,7 +73,7 @@ namespace AppointmentScheduling
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Account}/{action=Login}");
             });
         }
     }
